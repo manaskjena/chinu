84294c94b2f52898d04093fc858518b4c003fc5d
manaskjena <141745417+manaskjena@users.noreply.github.com>
2024-07-20 11:08:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 hamburger = document.querySelector(".hamburger");
+let check=true;
    hamburger.onclick =function(){
     
     navBar =document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
-   
+   if(check){
+      let profile=document.querySelector(".container1-logo");  
+      profile.style.display='none';
+      check=false;
+    }
+    else {
+     let  profile=document.querySelector(".container1-logo");  
+      profile.style.display='flex';
+      check=true;
+    }
     
 }
 
